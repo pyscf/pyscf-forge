1c0aeb143d493bac50199614fb8c9f17458a617b
ajz34 <ajz34@outlook.com>
2021-05-20 20:42:02 +0800

    update todo list for further amendment of program

--- __init__.py
+++ __init__.py
@@ -10,15 +10,3 @@ def DFDH(mol: gto.Mole, *args, **kwargs):
         return RDFDH(mol, *args, **kwargs)
 
 
-# TODO LIST: auxiliary integral transformation
-# TODO (0) determine shape of cderi: (n, n, a) or (a, n, n)
-# TODO (1) write multiple interface of cderi in MO representation
-# TODO (1) benchmark speed of utilizing pyscf's outcore+nr_e2
-# TODO (3) handle nmo != nao
-# TODO (3) handle int2c2e linear dependent
-# TODO (3) handle int2c2e eig situation for nuclear coordinate derivative
-
-# TODO LIST: future works
-# TODO RIJONX
-# TODO RIJCOSX (sgx)
-# TODO near-canonical U matrix
