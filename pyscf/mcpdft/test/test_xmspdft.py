e84b7f848656efed3917e0ee1628a80278a6e580
Matthew Hennefarth <matthew.hennefarth@gmail.com>
2023-02-28 14:45:54 -0600

    update the unit test for xmspdft to check the fock matrix as well

--- pyscf/mcpdft/test/test_xmspdft.py
+++ pyscf/mcpdft/test/test_xmspdft.py
@@ -1,37 +1,27 @@
 
 import numpy as np
-from math import cos, sin, pi
 from pyscf import gto, scf, mcpdft
+from pyscf.mcpdft import xmspdft
 
 import unittest
 
-def get_lih (r):
+def get_lih(r):
     mol = gto.M (atom='Li 0 0 0\nH {} 0 0'.format (r), basis='sto3g',
                  output='/dev/null', verbose=0)
     mf = scf.RHF (mol).run ()
-    mc = mcpdft.CASSCF (mf, 'tPBE', 2, 2, grids_level=1)
+    mc = mcpdft.CASSCF (mf, 'ftLDA,VWN3', 2, 2, grids_level=1)
     mc.fix_spin_(ss=0)
     mc = mc.multi_state ([0.5,0.5], 'xms').run (conv_tol=1e-8)
     return mol, mf, mc
 
-def get_h2(r):
-    mol = gto.M(atom='H 0 0 0\n H {} 0 0'.format(r), basis='sto3g', output='/dev/null', verbose=0)
-    mf = scf.RHF(mol).run()
-    mc = mcpdft.CASSCF(mf, 'tPBE', 2, 2, grids_level=6)
-    mc.fix_spin_(ss=0, shift=1)
-    mc = mc.multi_state([0.5, 0.5], 'xms').run(conv_tol=1e-8)
-    return mol, mf, mc
-
-
 def setUpModule():
-    global mol, mf, lih, h2
-    mol, mf, lih = get_lih (1.5)
-    _, _, h2 = get_h2(0.8)
+    global mol, mf, mc
+    mol, mf, mc = get_lih(1.5)
 
 def tearDownModule():
-    global mol, mf, lih, h2
-    mol.stdout.close ()
-    del mol, mf, lih, h2
+    global mol, mf, mc
+    mol.stdout.close()
+    del mol, mf, mc
 
 class KnownValues(unittest.TestCase):
 
@@ -40,34 +30,36 @@ class KnownValues(unittest.TestCase):
         for first, second in zip(first_list, second_list):
             self.assertAlmostEqual(first, second, expected)
 
+    # Reference values from
+    # - PySCF        hash 8ae2bb2eefcd342c52639097517b1eda7ca5d1cd
+    # - PySCF-forge  hash 5b8ab86a31917ca1a6b414f7a590c4046b9a8994
+    #
+    # Implementation with those hashes verified with OpenMolcas
+    # (tag 462-g00b34a15f)
+    def test_lih_diabats(self):
+        e_mcscf_avg = np.dot(mc.e_mcscf, mc.weights)
+        hcoup = abs(mc.heff_mcscf[1,0])
+        ct_mcscf = abs(mc.si_mcscf[0,0])
+
+        HCOUP_EXPECTED = 0.01996004651860848
+        CT_MCSCF_EXPECTED = 0.9886771524332543
+        E_MCSCF_AVG_EXPECTED = -7.789021830554006
+
+        self.assertAlmostEqual (e_mcscf_avg, E_MCSCF_AVG_EXPECTED, 9)
+        self.assertAlmostEqual (hcoup, HCOUP_EXPECTED, 9)
+        self.assertAlmostEqual (ct_mcscf, CT_MCSCF_EXPECTED, 9)
+
+    def test_lih_safock(self):
+        safock = xmspdft.make_fock_mcscf(mc, ci=mc.get_ci_adiabats(uci="MCSCF"))
+        EXPECTED_SA_FOCK_DIAG = [-4.207598506457942, -3.88169762424571]
+        EXPECTED_SA_FOCK_OFFDIAG = 0.05063053788053997
+
+        self.assertListAlmostEqual(safock.diagonal(), EXPECTED_SA_FOCK_DIAG, 9)
+        self.assertAlmostEqual(abs(safock[0,1]),EXPECTED_SA_FOCK_OFFDIAG, 9)
+
     def test_lih_adiabats(self):
-        e_mcscf_avg = np.dot (lih.e_mcscf, lih.weights)
-
-        hcoup = abs (lih.heff_mcscf[1,0])
-
-        E_MCSCF_EXPECTED = -7.78902185
-        E_STATES_EXPECTED = [-7.9108699492171475, -7.7549375652733685]
-        HCOUP_EXPECTED = 0.019960046518608488
-
-        self.assertAlmostEqual (e_mcscf_avg, E_MCSCF_EXPECTED, 7)
-        self.assertListAlmostEqual(lih.e_states, E_STATES_EXPECTED, 7)
-        self.assertAlmostEqual (hcoup, HCOUP_EXPECTED, 7)
-
-    def test_h2_adiabats(self):
-        e_mcscf_avg = np.dot(h2.e_mcscf, h2.weights)
-
-        # Reference Values from OpenMolcas v22.10
-        # tag 462-g00b34a15f
-        E_MCSCF_EXPECTED = -0.68103595
-        E_STATES_EXPECTED = [-1.15358469, -0.48247557]
-        HCOUP_EXPECTED = 0
-
-        hcoup = abs(h2.heff_mcscf[1, 0])
-
-        self.assertAlmostEqual(e_mcscf_avg, E_MCSCF_EXPECTED, 5)
-        self.assertListAlmostEqual(h2.e_states, E_STATES_EXPECTED, 5)
-        self.assertAlmostEqual(hcoup, HCOUP_EXPECTED, 5)
-
+        E_STATES_EXPECTED = [-7.858628517291297, -7.69980510010583]
+        self.assertListAlmostEqual(mc.e_states, E_STATES_EXPECTED, 9)
 
 if __name__ == "__main__":
     print("Full Tests for XMS-PDFT")
