8bb11f789782008128322bd212a47d137f96a615
Matthew Hennefarth <matthew.hennefarth@gmail.com>
2023-10-16 14:12:57 -0500

    update lpdft grad test, add mo hhe

--- pyscf/grad/test/test_grad_lpdft.py
+++ pyscf/grad/test/test_grad_lpdft.py
@@ -49,38 +49,14 @@ def diatomic(atom1, atom2, r, fnal, basis, ncas, nelecas, nstates,
     mc = mc.multi_state([1.0 / float(nstates), ] * nstates, 'lin')
     mc.fix_spin_(ss=ss, shift=2)
     mc.conv_tol = 1e-12
-    mc.conv_grad_tol = 1e-12
+    mc.conv_grad_tol = 1e-6
     mo = None
     if symmetry and (cas_irrep is not None):
         mo = mc.sort_mo_by_irrep(cas_irrep)
 
-    mc.kernel(mo)
-    return mc.nuc_grad_method()
-
-
-def auto_setup(xyz='Li 0 0 0\nH 1.5 0 0', symmetry=False, mix=False):
-    global mols
-    mol = gto.M(atom=xyz, basis='sto3g',
-                output='/dev/null', verbose=0, symmetry=symmetry)
-    mols.append(mol)
-    mf = scf.RHF(mol).run()
-    mc = mcpdft.CASSCF(mf, 'ftLDA,VWN3', 5, 2, grids_level=1).run()
-
-    if not mix:
-        return mc.multi_state([1.0 / 5, ] * 5, method="lin").run()
-
-    if symmetry:
-        solver_A1 = fci.solver(mol).set(wfnsym='A1', nroots=3)
-        solver_E1x = fci.solver(mol).set(wfnsym='E1x', nroots=1, spin=2)
-        solver_E1y = fci.solver(mol).set(wfnsym='E1y', nroots=1, spin=2)
-
-        solvers = [solver_A1, solver_E1x, solver_E1y]
-    else:
-        solver_S = fci.solver(mol, singlet=True).set(spin=0, nroots=2)
-        solver_T = fci.solver(mol, singlet=False).set(spin=2, nroots=3)
-        solvers = [solver_S, solver_T]
-
-    return mc.multi_state_mix(solvers, [1.0 / 5, ] * 5, method="lin").run()
+    mc_grad = mc.run(mo).nuc_grad_method()
+    mc_grad.conv_rtol = 1e-12
+    return mc_grad
 
 
 def setUpModule():
@@ -96,133 +72,23 @@ def tearDownModule():
 
 class KnownValues(unittest.TestCase):
 
-    def test_grad_h2_lin3ftlda22_sto3g_slow(self):
-        """ System has the following Lagrange multiplier sectors:
-            orb:    no
-            ci:     no
-        """
-        mc_grad = diatomic('H', 'H', 1.3, 'ftLDA,VWN3', 'STO-3G', 2, 2, 3)
-
-        # Numerical from this software
-        # PySCF commit:         824218f997
-        # PySCF-forge commit:   426bfa33e1
-        NUM_REF = [0.2483709972, -0.235934352, -0.7202397013]
-        for i in range(3):
-            with self.subTest(state=i):
-                de = mc_grad.kernel(state=i)[1, 0] / BOHR
-                self.assertAlmostEqual(de, NUM_REF[i], 7)
-
-    def test_grad_h2_lin2ftlda22_sto3g_slow(self):
-        """ System has the following Lagrange multiplier sectors:
-            orb:    no
-            ci:     yes
-        """
-        mc_grad = diatomic('H', 'H', 1.3, 'ftLDA,VWN3', 'STO-3G', 2, 2, 2)
-
-        # Numerical from this software
-        # PySCF commit:         824218f997
-        # PySCF-forge commit:   426bfa33e1
-        NUM_REF = [0.3045653798, -0.2328036389]
-        for i in range(2):
-            with self.subTest(state=i):
-                de = mc_grad.kernel(state=i)[1, 0] / BOHR
-                self.assertAlmostEqual(de, NUM_REF[i], 7)
-
-    def test_grad_h2_lin3ftlda22_631g_slow(self):
+    def test_grad_hhe_lin3ftlda22_631g_slow(self):
         """ System has the following Lagrange multiplier sectors:
             orb:    yes
             ci:     no
         """
-        mc_grad = diatomic('H', 'H', 1.3, 'ftLDA,VWN3', '6-31G', 2, 2, 3)
+        n_states=3
+        mc_grad = diatomic('He', 'H', 1.4, 'ftLDA,VWN3', '6-31G', 2, 2, n_states, charge=1)
 
         # Numerical from this software
-        # PySCF commit:         824218f997
-        # PySCF-forge commit:   426bfa33e1
-        NUM_REF = [0.1892819283, -0.1459936283, -0.4812691849]
-        for i in range(3):
+        # PySCF commit:         6c1ea86eb60b9527d6731efa65ef99a66b8f84d2
+        # PySCF-forge commit:   ea0a4c164de21e84eeb30007afcb45344cfc04ff
+        NUM_REF = [-0.0744181053, -0.0840211222, -0.0936241392]
+        for i in range(n_states):
             with self.subTest(state=i):
-                de = mc_grad.kernel(state=i)[1, 0] / BOHR
+                de = mc_grad.kernel(state=i)[1, 0]
                 self.assertAlmostEqual(de, NUM_REF[i], 7)
 
-    def test_grad_h2_lin2ftlda22_631g(self):
-        """ System has the following Lagrange multiplier sectors:
-            orb:    yes
-            ci:     yes
-        """
-        mc_grad = diatomic('H', 'H', 1.3, 'ftLDA,VWN3', '6-31G', 2, 2, 2)
-
-        # Numerical from this software
-        # PySCF commit:         824218f997
-        # PySCF-forge commit:   426bfa33e1
-        NUM_REF = [0.2181136615, -0.1549280412]
-        for i in range(2):
-            with self.subTest(state=i):
-                de = mc_grad.kernel(state=i)[1, 0] / BOHR
-                self.assertAlmostEqual(de, NUM_REF[i], delta=1e-5)
-
-    def test_grad_lih_lin2ftlda44_sto3g_slow(self):
-        """ System has the following Lagrange multiplier sectors:
-            orb:    no
-            ci:     yes
-        """
-        mc_grad = diatomic('Li', 'H', 1.8, 'ftLDA,VWN3', 'STO-3G', 4, 4, 2, symmetry=True, cas_irrep={'A1': 4})
-
-        # Numerical from this software
-        # PySCF commit:         824218f997
-        # PySCF-forge commit:   426bfa33e1
-        NUM_REF = [0.0706137599, -0.0065958760]
-        for i in range(2):
-            with self.subTest(state=i):
-                de = mc_grad.kernel(state=i)[1, 0] / BOHR
-                self.assertAlmostEqual(de, NUM_REF[i], delta=1e-6)
-
-    def test_grad_lih_lin3ftlda22_sto3g_slow(self):
-        """ System has the following Lagrange multiplier sectors:
-            orb:    yes
-            ci:     no
-        """
-        mc_grad = diatomic('Li', 'H', 2.4, 'ftLDA,VWN3', 'STO-3G', 2, 2, 3)
-
-        # Numerical from this software
-        # PySCF commit:         824218f997
-        # PySCF-forge commit:   426bfa33e1
-        NUM_REF = [0.1258966287, 0.0072754131, -0.1113474634]
-        for i in range(3):
-            with self.subTest(state=i):
-                de = mc_grad.kernel(state=i)[1, 0] / BOHR
-                self.assertAlmostEqual(de, NUM_REF[i], delta=1e-4)
-
-    def test_grad_lih_lin2ftlda22_sto3g_slow(self):
-        """ System has the following Lagrange multiplier sectors:
-            orb:    yes
-            ci:     yes
-        """
-        mc_grad = diatomic('Li', 'H', 2.4, 'ftLDA,VWN3', 'STO-3G', 2, 2, 2)
-
-        # Numerical from this software
-        # PySCF commit:         824218f997
-        # PySCF-forge commit:   426bfa33e1
-        NUM_REF = [0.1072597373, 0.0235666209]
-        for i in range(2):
-            with self.subTest(state=i):
-                de = mc_grad.kernel(state=i)[1, 0] / BOHR
-                self.assertAlmostEqual(de, NUM_REF[i], delta=1e-4)
-
-    def test_grad_lih_lin2ftpbe22_sto3g(self):
-        """ System has the following Lagrange multiplier sectors:
-            orb:    yes
-            ci:     yes
-        """
-        mc_grad = diatomic('Li', 'H', 2.4, 'ftPBE', 'STO-3G', 2, 2, 2)
-
-        # Numerical from this software
-        # PySCF commit:         824218f997
-        # PySCF-forge commit:   426bfa33e1
-        NUM_REF = [0.1024946074, 0.0190815023]
-        for i in range(2):
-            with self.subTest(state=i):
-                de = mc_grad.kernel(state=i)[1, 0] / BOHR
-                self.assertAlmostEqual(de, NUM_REF[i], delta=1e-4)
 
     def test_grad_scanner(self):
         # Tests API and Scanner capabilities
@@ -239,18 +105,6 @@ class KnownValues(unittest.TestCase):
                 self.assertTrue(mc_grad2.converged)
                 self.assertAlmostEqual(e1, e2, 6)
                 self.assertAlmostEqual(lib.fp(de1), lib.fp(de2), 6)
-
-    # Need to debug. Something not right with CI Lagrange multiplier terms...Maybe projection issue??
-    # def test_grad_multi_state_mix(self):
-    #     mc_nosym = auto_setup(mix=True, symmetry=False).nuc_grad_method()
-    #     mc_sym = auto_setup(mix=True, symmetry=True).nuc_grad_method()
-    #
-    #     for state in range(5):
-    #         state_nosym = np.argsort(mc_nosym.e_states)[state]
-    #         state_sym = np.argsort(mc_sym.e_states)[state]
-    #         de1 = mc_nosym.kernel(state=state_nosym)[0,0]
-    #         de2 = mc_sym.kernel(state=state_sym)[0,0]
-    #         print(f"de1: {de1} de2: {de2} diff:\t{de1-de2}")
 
 
 if __name__ == "__main__":
