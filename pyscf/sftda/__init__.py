2ef4c002985d8d234e7e7720fad3affa0cfedce5
Hao Li <86696701+Haskiy@users.noreply.github.com>
2024-05-22 11:38:00 +0800

    Spin Flip TDA/ TDDFT using Multicollinear Approach. (#48)
    * pretest
    
    * Add the sftda dic, containing SF-TDA/ TDDFT.
    
    * Add the top comment for files and a spin flip tda calculation example.
    
    * copy __mcfun_fn_eval_xc to numint2c_sftd.py.
    
    * define fucntion get_ab_sf() as TDA_SF class function.
    
    * Modified the mistakes in CI/Lint tests.
    
    * Skip the unittests if mcfun isn't installed.
    
    * lint errors
    
    ---------
    
    Co-authored-by: maohaohao <lihao@stu.pku.edu.cn>
    Co-authored-by: Qiming Sun <osirpt.sun@gmail.com>

---
+++ pyscf/sftda/__init__.py
@@ -0,0 +1,35 @@
+#/usr/bin/env python
+# Copyright 2014-2024 The PySCF Developers. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from pyscf import scf
+from pyscf.dft import KohnShamDFT
+from pyscf.sftda import uks_sf
+from pyscf.sftda.uks_sf import TDUKS_SF
+
+
+def TDA_SF(mf):
+    mf = mf.remove_soscf()
+    if isinstance(mf, scf.rohf.ROHF):
+        if isinstance(mf, KohnShamDFT):
+            mf = mf.to_uks()
+        else:
+            mf = mf.to_uhf()
+    return mf.TDA_SF()
+
+def TDDFT_SF(mf):
+    print('Warning!!! SF-TDDFT ruining in the slow divergence, ' + \
+          'you can choose get_ab_sf() to construct the full matrix ' + \
+          'to obtain the excited energies.')
+    return mf.TDDFT_SF()
