9f2ed59167e853dc94ab8b52dc85ef634ea721fa
Dayou Zhang <zhan6350@umn.edu>
2024-09-13 17:08:49 -0500

    new mcdcft implementation

--- pyscf/mcdcft/test/test_energy_h2.py
+++ pyscf/mcdcft/test/test_energy_h2.py
@@ -20,13 +20,13 @@ import unittest
 import tempfile
 import os
 
-def run(r, xc, ot_name, chkfile):
+def run(r, xc, display_name, chkfile):
     r /= 2
-    mol = gto.M(atom=f'H  0 0 {r}; H 0 0 -{r}', basis='cc-pvtz', 
+    mol = gto.M(atom=f'H  0 0 {r}; H 0 0 -{r}', basis='cc-pvtz',
           symmetry=False, verbose=0)
     mf = scf.RHF(mol)
     mf.kernel()
-    mc = mcdcft.CASSCF(mf, xc, 2, 2, ot_name=ot_name, 
+    mc = mcdcft.CASSCF(mf, xc, 2, 2, display_name=display_name,
                        grids_level=6)
     #mc.fcisolver = csf_solver(mol, smult=1)
     mc.fix_spin_(ss=0)
@@ -35,13 +35,29 @@ def run(r, xc, ot_name, chkfile):
     mc.dump_mcdcft_chk(chkfile)
     return mc.e_tot
 
-def restart(xc, ot_name, chkfile):
+def run2(r, xc, display_name, chkfile):
+    r /= 2
+    mol = gto.M(atom=f'H  0 0 {r}; H 0 0 -{r}', basis='cc-pvtz',
+          symmetry=False, verbose=0)
+    mf = scf.RHF(mol)
+    mf.kernel()
+    #  xc0 = 'B1LYP'
+    xc0 = 'LDA'
+    mc = mcdcft.CASSCF(mf, xc0, 2, 2, display_name=display_name,
+                       grids_level=6)
+    #mc.fcisolver = csf_solver(mol, smult=1)
+    mc.fix_spin_(ss=0)
+    mc.kernel()
+    mc.recalculate_with_xc(xc, display_name=display_name, load_chk=chkfile)
+    return mc.e_tot
+
+def restart(xc, display_name, chkfile):
     mol = lib.chkfile.load_mol(chkfile)
     mol.verbose = 0
     mf = scf.RHF(mol)
-    mc = mcdcft.CASSCF(mf, None, 2, 2, grids_level=6)
+    mc = mcdcft.CASSCF(mf, xc, 2, 2, grids_level=6)
     mc.load_mcdcft_chk(chkfile)
-    mc.recalculate_with_xc(xc, ot_name=ot_name, dump_chk=chkfile)
+    mc.recalculate_with_xc(xc, display_name=display_name, dump_chk=chkfile)
     return mc.e_tot
 
 class KnownValues(unittest.TestCase):
@@ -53,6 +69,8 @@ class KnownValues(unittest.TestCase):
                 chkname2 = chkfile2.name
                 self.assertAlmostEqual(run(8.00, 'PBE', 'cPBE', chkname1) -
                                        run(0.78, 'PBE', 'cPBE', chkname2), 0.14898997201251052, 5)
+                self.assertAlmostEqual(run2(8.00, 'PBE', 'cPBE', chkname1) -
+                                       run2(0.78, 'PBE', 'cPBE', chkname2), 0.14898997201251052, 5)
                 self.assertAlmostEqual(restart('BLYP', 'cBLYP', chkname1) -
                                        restart('BLYP', 'cBLYP', chkname2), 0.15624825293702616, 5)
 
@@ -63,6 +81,8 @@ class KnownValues(unittest.TestCase):
                 chkname2 = chkfile2.name
                 self.assertAlmostEqual(run(8.00, 'BLYP', 'cBLYP', chkname1) -
                                        run(0.78, 'BLYP', 'cBLYP', chkname2), 0.15624825293702616, 5)
+                self.assertAlmostEqual(run2(8.00, 'BLYP', 'cBLYP', chkname1) -
+                                       run2(0.78, 'BLYP', 'cBLYP', chkname2), 0.15624825293702616, 5)
                 self.assertAlmostEqual(restart('PBE', 'cPBE', chkname1) -
                                        restart('PBE', 'cPBE', chkname2), 0.14898997201251052, 5)
 
