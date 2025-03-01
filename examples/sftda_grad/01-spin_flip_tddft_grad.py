b9562c94acb065bbc53b194769970711691988a1
maohaohao <lihao@stu.pku.edu.cn>
2025-03-01 15:13:16 +0800

    Add the unittest and example for sftda_grad module.

--- examples/sftda_grad/01-spin_flip_tddft_grad.py
+++ examples/sftda_grad/01-spin_flip_tddft_grad.py
@@ -1,62 +1,34 @@
-#!/usr/bin/env/python
-
 '''
-Test transition_analyze function in sftda.tool_td.py.
+For spin-flip TDDFT gradient, you can obtain and create a spin-flip TDDFT
+object use sftda.uhf_df.get_ab_sf() function. And transfrom the excited 
+energy \omega and transition vector x,y into the correct form. Then use
+the gradient module.
 '''
-
 import numpy as np
 from pyscf import lib, gto, dft
 from pyscf import sftda
-from pyscf.sftda import tools_td
-
+from pyscf.sftda_grad import tduks_sf # this import is necessary.
+try:
+    import mcfun # mcfun>=0.2.5 must be used.
+except ImportError:
+    mcfun = None
+    
 mol = gto.Mole()
 mol.verbose = 6
 mol.output = None
 mol.spin = 2
 mol.atom = 'O 0 0 2.07; O 0 0 0'
 mol.unit = 'B'
-mol.basis = '631g'
+mol.basis = '6-31g'
 mol.build()
 
+# UKS object
 mf = dft.UKS(mol)
 mf.xc = 'svwn' # blyp, b3lyp, tpss
 mf.kernel()
 
-mftd1 = sftda.TDA_SF(mf)
-mftd1.nstates = 5
-mftd1.extype = 0
-mftd1.collinear_samples=200
-mftd1.kernel()
-
-e = mftd1.e
-xy = mftd1.xy
-
-#
-# 1. Print tansition analyze for the first spin-flip up excited state.
-#
-tools_td.transition_analyze(mf, mftd1, e[0], xy[0], tdtype='TDA')
-
-
-mftd2 = sftda.uks_sf.TDA_SF(mf)
-mftd2.nstates = 5
-mftd2.extype = 1
-mftd2.collinear_samples=200
-mftd2.kernel()
-
-e = mftd2.e
-xy = mftd2.xy
-
-#
-# 2. Print tansition analyze for the first spin-flip down excited state.
-#
-for i in range(5):
-    tools_td.transition_analyze(mf, mftd2, e[i], xy[i], tdtype='TDA')
-
-
-#
-# 3. Print tansition analyze for the first spin-flip down excited state using TDDFT.
-#
-a, b = sftda.TDDFT_SF(mf).get_ab_sf(collinear_samples=200)
+# use get_ab_sf() to get response Matrix
+a, b = sftda.TDDFT_SF(mf).get_ab_sf()
 A_baba, A_abab = a
 B_baab, B_abba = b
 
@@ -71,17 +43,20 @@ A_baba_2d = A_baba.reshape((n_occ_b*n_virt_a,n_occ_b*n_virt_a), order='C')
 Casida_matrix = np.block([[ A_abab_2d, B_abba_2d],
                           [-B_baab_2d,-A_baba_2d]])
 
+# solve the response matrix
 eigenvals, eigenvecs = np.linalg.eig(Casida_matrix)
 
 # sort the eigenvalues and eigenvectors
 idxt = eigenvals.real.argsort()
 eigenvals = eigenvals[idxt]
 eigenvecs = eigenvecs[:, idxt]
+
 # find the positive roots
 idxp = np.where(eigenvals>-1e-4)[0]
 eigenvals = eigenvals[idxp]
-eigenvecs = eigenvecs[:, idxp].transpose(1,0)
+eigenvecs= eigenvecs[:, idxp].transpose(1,0)
 
+# transfrom the form of transiton vector
 def norm_xy(z):
     x = z[:n_occ_a*n_virt_b].reshape(n_occ_a,n_virt_b)
     y = z[n_occ_a*n_virt_b:].reshape(n_occ_b,n_virt_a)
@@ -89,13 +64,17 @@ def norm_xy(z):
     norm = np.sqrt(1./norm)
     return ((0,x*norm), (y*norm,0))
 
-class CasidaSolution:
-    pass
+# transfrom the form of transiton vector
+mftd1 = sftda.TDA_SF(mf)
+mftd1.nstates = 5 # the number of excited states
+mftd1.extype = 1  # 1 for spin flip down excited energies
+mftd1.collinear_samples=200
+mftd1.e = eigenvals
+mftd1.xy = [norm_xy(z) for z in eigenvecs[:5]]
 
-mfcasida = CasidaSolution()
-mfcasida.extype = 1
-mfcasida.e = eigenvals
-mfcasida.xy = [norm_xy(z) for z in eigenvecs[:5]]
+# print(mftd1.e)
 
-for i in range(5):
-    tools_td.transition_analyze(mf, mfcasida, mfcasida.e[i], mfcasida.xy[i], tdtype='TDDFT')
+mftdg1 = mftd1.Gradients()
+g1 = mftdg1.kernel(state=2)
+
+print(g1)
--- pyscf/sftda_grad/test/uks_sf_tda_grad.py
+++ pyscf/sftda_grad/test/uks_sf_tda_grad.py
@@ -1,35 +1,118 @@
-from pyscf import lib, gto, scf, dft
-from pyscf import sftda,tdscf
-from pyscf.grad import tduks
+#/usr/bin/env python
+# Copyright 2014-2024 The PySCF Developers. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import unittest
+from pyscf import gto, dft
+from pyscf import sftda
 from pyscf.sftda_grad import tduks_sf
 try:
     import mcfun
 except ImportError:
     mcfun = None
-    
-mol = gto.Mole()
-mol.verbose = 6
-mol.output = None
-mol.spin = 2
-mol.atom = 'O 0 0 2.07; O 0 0 0'
-mol.unit = 'B'
-mol.basis = 'sto-3g'
-mol.build()
 
-mf = dft.UKS(mol)
-mf.xc = 'svwn' # blyp, b3lyp, tpss
-mf.kernel()
+# ToDo: Add the SF-TDDFT Grad tests.
 
-mftd1 = sftda.TDA_SF(mf)
-# mftd1 = tdscf.TDA(mf)
-mftd1.nstates = 5 # the number of excited states
-mftd1.extype = 1 # 0 for spin flip up excited energies
-mftd1.collinear_samples=200
-mftd1.kernel()
+def setUpModule():
+    global mol, pmol, mf_lda, mf_bp86, mf_b3lyp, mf_tpss, nstates
+    mol = gto.Mole()
+    mol.verbose = 5
+    mol.output = '/dev/null'
+    mol.atom = [
+        ['O' , (0. , 0. , 2.070)],
+        ['O' , (0. , 0. , 0.)], ]
+    mol.unit = 'B'
+    mol.spin = 2
+    mol.basis = '6-31g'
+    mol.build()
+    pmol = mol.copy()
 
-mftdg1 = mftd1.Gradients()
-# mftdg1 = mftd1.nuc_grad_method()
-g1 = mftdg1.kernel(state=2)
+    mf_lda  = dft.UKS(mol).set(xc='lda,', conv_tol=1e-12)
+    mf_lda.kernel()
+    mf_bp86 = dft.UKS(mol).set(xc='bp86', conv_tol=1e-12)
+    mf_bp86.kernel()
+    mf_b3lyp= dft.UKS(mol).set(xc='b3lyp',conv_tol=1e-12)
+    mf_b3lyp.kernel()
+    mf_tpss = dft.UKS(mol).set(xc='tpss', conv_tol=1e-12)
+    mf_tpss.kernel()
 
-print('***********************')
-print(g1)
+def tearDownModule():
+    global mol, pmol, mf_lda, mf_bp86, mf_b3lyp, mf_tpss
+    mol.stdout.close()
+    del mol, pmol, mf_lda, mf_bp86, mf_b3lyp, mf_tpss
+
+class KnownValues(unittest.TestCase):
+    def test_tdasfd_anag_lda(self):
+        td = sftda.TDA_SF(mf_lda).set(conv_tol=1e-12)
+        td.extype = 1
+        td.collinear_samples = 200
+        td.kernel(nstates=5)[0]
+
+        tdg = td.nuc_grad_method()
+        g1 = tdg.kernel(td.xy[2])
+        self.assertAlmostEqual(g1[0,2], -0.37025743446132253, 6)
+
+        td_solver = td.as_scanner()
+        e1 = td_solver(pmol.set_geom_('O 0 0 2.071; O 0 0 0', unit='B'))
+        e2 = td_solver(pmol.set_geom_('O 0 0 2.069; O 0 0 0', unit='B'))
+        self.assertAlmostEqual((e1[2]-e2[2])/.002, g1[0,2], 4)
+
+    def test_tdasfu_anag_bp86(self):
+        td = sftda.TDA_SF(mf_bp86).set(conv_tol=1e-12)
+        td.extype = 0
+        td.collinear_samples = 200
+        td.kernel(nstates=5)[0]
+
+        tdg = td.nuc_grad_method()
+        g1 = tdg.kernel(td.xy[2])
+        self.assertAlmostEqual(g1[0,2], -1.4387734863741706, 6)
+
+        td_solver = td.as_scanner()
+        e1 = td_solver(pmol.set_geom_('O 0 0 2.071; O 0 0 0', unit='B'))
+        e2 = td_solver(pmol.set_geom_('O 0 0 2.069; O 0 0 0', unit='B'))
+        self.assertAlmostEqual((e1[2]-e2[2])/.002, g1[0,2], 4)
+
+    def test_tdasfu_anag_b3lyp(self):
+        td = sftda.TDA_SF(mf_b3lyp).set(conv_tol=1e-12)
+        td.extype = 1
+        td.collinear_samples = 200
+        td.kernel(nstates=5)[0]
+
+        tdg = td.Gradients()
+        g1 = tdg.kernel(td.xy[3])
+        self.assertAlmostEqual(g1[0,2], -0.3428495646475067, 6)
+
+        td_solver = td.as_scanner()
+        e1 = td_solver(pmol.set_geom_('O 0 0 2.071; O 0 0 0', unit='B'))
+        e2 = td_solver(pmol.set_geom_('O 0 0 2.069; O 0 0 0', unit='B'))
+        self.assertAlmostEqual((e1[3]-e2[3])/.002, g1[0,2], 4)
+
+    def test_tdasfu_anag_tpss(self):
+        td = sftda.TDA_SF(mf_tpss).set(conv_tol=1e-12)
+        td.extype = 0
+        td.collinear_samples = 200
+        td.kernel(nstates=5)[0]
+
+        tdg = td.Gradients()
+        g1 = tdg.kernel(td.xy[3])
+        self.assertAlmostEqual(g1[0,2], -1.027043808560098, 6)
+
+        td_solver = td.as_scanner()
+        e1 = td_solver(pmol.set_geom_('O 0 0 2.071; O 0 0 0', unit='B'))
+        e2 = td_solver(pmol.set_geom_('O 0 0 2.069; O 0 0 0', unit='B'))
+        self.assertAlmostEqual((e1[3]-e2[3])/.002, g1[0,2], 4)
+
+if __name__ == "__main__":
+    print("Full Tests for SF-TD-UKS gradients")
+    unittest.main()
