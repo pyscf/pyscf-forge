5a09d7ff3c25a73e103f6551e9042f8a5622af5f
Matthew R Hermes <mrhermes@uchicago.edu>
2025-04-22 10:29:18 -0500

    Transfer CSF FCI from mrh to pyscf-forge

---
+++ pyscf/csf_fci/__init__.py
@@ -0,0 +1,13 @@
+from mrh.my_pyscf.fci import csf, csf_symm
+from mrh.my_pyscf.fci.csfstring import CSFTransformer
+
+CSFFCISolver = csf.CSFFCISolver
+
+def csf_solver(mol=None, smult=None, symm=None):
+    if mol and symm is None:
+        symm = mol.symmetry
+    if symm:
+        return csf_symm.FCISolver (mol=mol, smult=smult)
+    else:
+        return csf.FCISolver (mol=mol, smult=smult)
+
