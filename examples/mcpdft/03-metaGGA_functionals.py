ec4866718205ab70714e820a04997994cf6b1883
Matthew R Hermes <mrhermes@uchicago.edu>
2025-07-17 11:22:45 -0500

    Remove files being moved to PySCF core (#144)

--- examples/mcpdft/03-metaGGA_functionals.py
+++ examples/mcpdft/03-metaGGA_functionals.py
@@ -8,15 +8,6 @@ mol = gto.M (
 
 mf = scf.RHF (mol).run ()
 
-# The translation of Meta-GGAs and hybrid-Meta-GGAs [PNAS, 122, 1, 2025, e2419413121; https://doi.org/10.1073/pnas.2419413121]
-
-# Translated-Meta-GGA
-mc = mcpdft.CASCI(mf, 'tM06L', 6, 8).run ()
-
-# Hybrid-Translated-Meta-GGA
-tM06L0 = 't' + mcpdft.hyb('M06L',0.25, hyb_type='average')
-mc = mcpdft.CASCI(mf, tM06L0, 6, 8).run ()
-
 # MC23: meta-hybrid on-top functional [PNAS, 122, 1, 2025, e2419413121; https://doi.org/10.1073/pnas.2419413121]
 
 # State-Specific
