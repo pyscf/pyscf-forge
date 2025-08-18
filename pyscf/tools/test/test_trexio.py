2ac74c53b44b9e8aad93d77a47063813834e29ed
kousuke-nakano <kousuke.kyo.u@gmail.com>
2025-08-18 16:17:24 +0900

    Improved test_trexio

--- pyscf/tools/test/test_trexio.py
+++ pyscf/tools/test/test_trexio.py
@@ -1,117 +1,174 @@
 import pyscf
 from pyscf.tools import trexio
+import os
+import tempfile
+import pytest
 
-def test_mol_ae_6_31g():
-    filename = 'test_mol_ae_6_31g_sphe.h5'
-    mol = pyscf.M(atom='H 0 0 0; F 0 0 1', basis='6-31g**', cart=False)
-    ref = mol.intor('int1e_ovlp')
-    trexio.to_trexio(mol, filename)
-    mol = trexio.mol_from_trexio(filename)
-    s1 = mol.intor('int1e_ovlp')
-    assert abs(ref - s1).max() < 1e-12
+DIFF_TOL = 1e-10
 
-    filename = 'test_mol_ae_6_31g_cart.h5'
-    mol = pyscf.M(atom='H 0 0 0; F 0 0 1', basis='6-31g**', cart=True)
-    ref = mol.intor('int1e_ovlp')
-    trexio.to_trexio(mol, filename)
-    mol = trexio.mol_from_trexio(filename)
-    s1 = mol.intor('int1e_ovlp')
-    assert abs(ref - s1).max() < 1e-12
+#################################################################
+# reading/writing `mol` from/to trexio file
+#################################################################
 
-def test_mol_general_contraction():
-    filename = 'test_mol_general_contraction_sphe.h5'
-    mol = pyscf.M(atom='C', basis='ano')
-    ref = mol.intor('int1e_ovlp')
-    trexio.to_trexio(mol, filename)
-    mol = trexio.mol_from_trexio(filename)
-    s1 = mol.intor('int1e_ovlp')
-    assert abs(ref - s1).max() < 1e-12
+## molecule, segment contraction (6-31g), all-electron
+@pytest.mark.parametrize("cart", [False, True], ids=["cart=false", "cart=true"])
+def test_mol_ae_6_31g(cart):
+    with tempfile.TemporaryDirectory() as d:
+        filename = os.path.join(d, 'test.h5')
+        mol0 = pyscf.M(atom='H 0 0 0; F 0 0 1', basis='6-31g**', cart=cart)
+        s0 = mol0.intor('int1e_ovlp')
+        t0 = mol0.intor('int1e_kin')
+        v0 = mol0.intor('int1e_nuc')
+        trexio.to_trexio(mol0, filename)
+        mol1 = trexio.mol_from_trexio(filename)
+        s1 = mol1.intor('int1e_ovlp')
+        t1 = mol1.intor('int1e_kin')
+        v1 = mol1.intor('int1e_nuc')
+        assert abs(s0 - s1).max() < DIFF_TOL
+        assert abs(t0 - t1).max() < DIFF_TOL
+        assert abs(v0 - v1).max() < DIFF_TOL
 
-def test_mol_ccecp_ccpvqz():
-    filename = 'test_mol_ccecp_ccpvqz_sphe.h5'
-    mol = pyscf.M(atom='H 0 0 0; F 0 0 1', basis='ccecp-cc-pVQZ', ecp='ccECP', cart=False)
-    # ref = mol.intor('int1e_ovlp')
-    trexio.to_trexio(mol, filename)
-    ''' Todo: mol_from_trexio for ecp is yet implemented.
-    mol = trexio.mol_from_trexio(filename)
-    s1 = mol.intor('int1e_ovlp')
-    assert abs(ref - s1).max() < 1e-12
-    '''
+## molecule, general contraction (ccpv5z), all-electron
+@pytest.mark.parametrize("cart", [False, True], ids=["cart=false", "cart=true"])
+def test_mol_ae_ccpv5z(cart):
+    with tempfile.TemporaryDirectory() as d:
+        filename = os.path.join(d, 'test.h5')
+        mol0 = pyscf.M(atom='C', basis='ccpv5z', cart=cart)
+        s0 = mol0.intor('int1e_ovlp')
+        t0 = mol0.intor('int1e_kin')
+        v0 = mol0.intor('int1e_nuc')
+        trexio.to_trexio(mol0, filename)
+        mol1 = trexio.mol_from_trexio(filename)
+        s1 = mol1.intor('int1e_ovlp')
+        t1 = mol1.intor('int1e_kin')
+        v1 = mol1.intor('int1e_nuc')
+        assert abs(s0 - s1).max() < DIFF_TOL
+        assert abs(t0 - t1).max() < DIFF_TOL
+        assert abs(v0 - v1).max() < DIFF_TOL
 
-    filename = 'test_mol_ccecp_ccpvqz_cart.h5'
-    mol = pyscf.M(atom='H 0 0 0; F 0 0 1', basis='ccecp-cc-pVQZ', ecp='ccECP', cart=True)
-    # ref = mol.intor('int1e_ovlp')
-    trexio.to_trexio(mol, filename)
-    ''' Todo: mol_from_trexio for ecp is yet implemented.
-    mol = trexio.mol_from_trexio(filename)
-    s1 = mol.intor('int1e_ovlp')
-    assert abs(ref - s1).max() < 1e-12
-    '''
+## molecule, general contraction (ano), all-electron
+@pytest.mark.parametrize("cart", [False, True], ids=["cart=false", "cart=true"])
+def test_mol_ae_ano(cart):
+    with tempfile.TemporaryDirectory() as d:
+        filename = os.path.join(d, 'test.h5')
+        mol0 = pyscf.M(atom='C', basis='ano', cart=cart)
+        s0 = mol0.intor('int1e_ovlp')
+        t0 = mol0.intor('int1e_kin')
+        v0 = mol0.intor('int1e_nuc')
+        trexio.to_trexio(mol0, filename)
+        mol1 = trexio.mol_from_trexio(filename)
+        s1 = mol1.intor('int1e_ovlp')
+        t1 = mol1.intor('int1e_kin')
+        v1 = mol1.intor('int1e_nuc')
+        assert abs(s0 - s1).max() < DIFF_TOL
+        assert abs(t0 - t1).max() < DIFF_TOL
+        assert abs(v0 - v1).max() < DIFF_TOL
 
-def test_mol_ae_ccpvdz():
-    filename = 'test_mol_ae_ccpvdz_sphe.h5'
-    mol = pyscf.M(atom='H 0 0 0; F 0 0 1', basis='ccpvdz', cart=False)
-    ref = mol.intor('int1e_ovlp')
-    trexio.to_trexio(mol, filename)
-    mol = trexio.mol_from_trexio(filename)
-    s1 = mol.intor('int1e_ovlp')
-    assert abs(ref - s1).max() < 1e-12
+## molecule, segment contraction (ccecp-cc-pVQZ), ccecp
+@pytest.mark.parametrize("cart", [False, True], ids=["cart=false", "cart=true"])
+def test_mol_ccecp_ccecp_ccpvqz(cart):
+    with tempfile.TemporaryDirectory() as d:
+        filename = os.path.join(d, 'test.h5')
+        mol0 = pyscf.M(atom='H 0 0 0; F 0 0 1', basis='ccecp-ccpvqz', ecp='ccecp', cart=cart)
+        s0 = mol0.intor('int1e_ovlp')
+        t0 = mol0.intor('int1e_kin')
+        v0 = mol0.intor('int1e_nuc')
+        trexio.to_trexio(mol0, filename)
+        mol1 = trexio.mol_from_trexio(filename)
+        s1 = mol1.intor('int1e_ovlp')
+        t1 = mol1.intor('int1e_kin')
+        v1 = mol1.intor('int1e_nuc')
+        assert abs(s0 - s1).max() < DIFF_TOL
+        assert abs(t0 - t1).max() < DIFF_TOL
+        assert abs(v0 - v1).max() < DIFF_TOL
 
-    filename = 'test_mol_ae_ccpvdz_cart.h5'
-    mol = pyscf.M(atom='H 0 0 0; F 0 0 1', basis='ccpvdz', cart=True)
-    ref = mol.intor('int1e_ovlp')
-    trexio.to_trexio(mol, filename)
-    mol = trexio.mol_from_trexio(filename)
-    s1 = mol.intor('int1e_ovlp')
-    assert abs(ref - s1).max() < 1e-12
+#################################################################
+# reading/writing `mf` from/to trexio file
+#################################################################
 
-def test_mf_ae_6_31g():
-    filename = 'test_mf_ae_6_31g_sphe.h5'
-    mol = pyscf.M(atom='H 0 0 0; F 0 0 1', basis='6-31g*', cart=False)
-    mf = mol.RHF().run()
-    #print(mf.mo_coeff)
-    trexio.to_trexio(mf, filename)
-    mf1 = trexio.scf_from_trexio(filename)
-    assert abs(mf1.mo_coeff - mf.mo_coeff).max() < 1e-12
-    trexio.write_eri(mf._eri, filename)
-    eri = trexio.read_eri(filename)
-    assert abs(mf._eri - eri).max() < 1e-12
+## molecule, segment contraction (6-31g), all-electron, RHF
+@pytest.mark.parametrize("cart", [False, True], ids=["cart=false", "cart=true"])
+def test_mf_rhf_ae_6_31g(cart):
+    with tempfile.TemporaryDirectory() as d:
+        filename = os.path.join(d, 'test.h5')
+        mol0 = pyscf.M(atom='H 0 0 0; F 0 0 1', basis='6-31g*', cart=cart)
+        mf0 = mol0.RHF().run()
+        eri0 = mf0._eri
+        trexio.to_trexio(mf0, filename)
+        mf1 = trexio.scf_from_trexio(filename)
+        assert abs(mf1.mo_coeff - mf0.mo_coeff).max() < DIFF_TOL
+        trexio.write_eri(eri0, filename)
+        eri1 = trexio.read_eri(filename)
+        assert abs(eri0 - eri1).max() < DIFF_TOL
 
-    filename = 'test_mf_ae_6_31g_cart.h5'
-    mol = pyscf.M(atom='H 0 0 0; F 0 0 1', basis='6-31g*', cart=True)
-    mf = mol.RHF().run()
-    #print(mf.mo_coeff)
-    trexio.to_trexio(mf, filename)
-    mf1 = trexio.scf_from_trexio(filename)
-    assert abs(mf1.mo_coeff - mf.mo_coeff).max() < 1e-12
-    trexio.write_eri(mf._eri, filename)
-    eri = trexio.read_eri(filename)
-    assert abs(mf._eri - eri).max() < 1e-12
+## molecule, segment contraction (ccecp-cc-pVQZ), ccecp, RHF
+@pytest.mark.parametrize("cart", [False, True], ids=["cart=false", "cart=true"])
+def test_mf_rhf_ccecp_ccpvqz(cart):
+    with tempfile.TemporaryDirectory() as d:
+        filename = os.path.join(d, 'test.h5')
+        mol0 = pyscf.M(atom='H 0 0 0; F 0 0 1', basis='ccecp-ccpvqz', ecp='ccecp', cart=cart)
+        mf0 = mol0.RHF().run()
+        eri0 = mf0._eri
+        trexio.to_trexio(mf0, filename)
+        mf1 = trexio.scf_from_trexio(filename)
+        assert abs(mf1.mo_coeff - mf0.mo_coeff).max() < DIFF_TOL
+        trexio.write_eri(eri0, filename)
+        eri1 = trexio.read_eri(filename)
+        assert abs(eri0 - eri1).max() < DIFF_TOL
 
-def test_mf_ccecp_ccpvqz():
-    filename = 'test_mf_ccecp_ccpvqz_sphe.h5'
-    mol = pyscf.M(atom='H 0 0 0; F 0 0 1', basis='ccecp-cc-pVQZ', ecp='ccECP', cart=False)
-    mf = mol.RHF().run()
-    trexio.to_trexio(mf, filename)
-    ''' Todo: scf_from_trexio for ecp is yet implemented.
-    mf1 = trexio.scf_from_trexio(filename)
-    assert abs(mf1.mo_coeff - mf.mo_coeff).max() < 1e-12
-    trexio.write_eri(mf._eri, filename)
-    eri = trexio.read_eri(filename)
-    assert abs(mf._eri - eri).max() < 1e-12
-    '''
+#################################################################
+# reading/writing `mol` from/to trexio file + SCF run.
+#################################################################
 
-    filename = 'test_mf_ccecp_ccpvqz_cart.h5'
-    mol = pyscf.M(atom='H 0 0 0; F 0 0 1', basis='ccecp-cc-pVQZ', ecp='ccECP', cart=True)
-    mf = mol.RHF().run()
-    trexio.to_trexio(mf, filename)
-    ''' Todo: scf_from_trexio for ecp is yet implemented.
-    mf1 = trexio.scf_from_trexio(filename)
-    assert abs(mf1.mo_coeff - mf.mo_coeff).max() < 1e-12
-    trexio.write_eri(mf._eri, filename)
-    eri = trexio.read_eri(filename)
-    assert abs(mf._eri - eri).max() < 1e-12
-    '''
+## molecule, segment contraction (6-31g), all-electron, RHF
+@pytest.mark.parametrize("cart", [False, True], ids=["cart=false", "cart=true"])
+def test_mol_scf_rhf_ae_6_31g(cart):
+    with tempfile.TemporaryDirectory() as d:
+        filename = os.path.join(d, 'test.h5')
+        mol0 = pyscf.M(atom='H 0 0 0; F 0 0 1', basis='6-31g*', cart=cart)
+        trexio.to_trexio(mol0, filename)
+        mf0 = mol0.RHF().run()
+        e0 = mf0.e_tot
+        mol1 = trexio.mol_from_trexio(filename)
+        mf1 = mol1.RHF().run()
+        e1 = mf1.e_tot
+        assert abs(e0 - e1).max() < DIFF_TOL
+
+## molecule, segment contraction (ccecp-cc-pVQZ), ccecp, RHF
+@pytest.mark.parametrize("cart", [False, True], ids=["cart=false", "cart=true"])
+def test_mol_rhf_ccecp_ccpvqz(cart):
+    with tempfile.TemporaryDirectory() as d:
+        filename = os.path.join(d, 'test.h5')
+        mol0 = pyscf.M(atom='H 0 0 0; F 0 0 1', basis='ccecp-ccpvqz', ecp='ccecp', cart=cart)
+        trexio.to_trexio(mol0, filename)
+        mf0 = mol0.RHF().run()
+        e0 = mf0.e_tot
+        mol1 = trexio.mol_from_trexio(filename)
+        mf1 = mol1.RHF().run()
+        e1 = mf1.e_tot
+        assert abs(e0 - e1).max() < DIFF_TOL
+
+    with tempfile.TemporaryDirectory() as d:
+        filename = os.path.join(d, 'test.h5')
+        mol0 = pyscf.M(atom='F 0 0 0; F 0 0 1', basis='ccecp-ccpvqz', ecp='ccecp', cart=cart)
+        trexio.to_trexio(mol0, filename)
+        mf0 = mol0.RHF().run()
+        e0 = mf0.e_tot
+        mol1 = trexio.mol_from_trexio(filename)
+        mf1 = mol1.RHF().run()
+        e1 = mf1.e_tot
+        assert abs(e0 - e1).max() < DIFF_TOL
+
+    with tempfile.TemporaryDirectory() as d:
+        filename = os.path.join(d, 'test.h5')
+        mol0 = pyscf.M(atom='H 0 0 0; H 0 0 1', basis='ccecp-ccpvqz', ecp='ccecp', cart=cart)
+        trexio.to_trexio(mol0, filename)
+        mf0 = mol0.RHF().run()
+        e0 = mf0.e_tot
+        mol1 = trexio.mol_from_trexio(filename)
+        mf1 = mol1.RHF().run()
+        e1 = mf1.e_tot
+        assert abs(e0 - e1).max() < DIFF_TOL
 
 if __name__ == "__main__":
     #test_mol_ae_6_31g()
