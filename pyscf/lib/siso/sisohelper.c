0d7b2811cc7a6edc0c1be103d7021e71b98ecd17
JangidBhavnesh <bhuvnesh.jangid10@gmail.com>
2026-08-12 20:28:52 -0500

    Cleanups the lower level SISO fns

--- pyscf/lib/siso/sisohelper.c
+++ pyscf/lib/siso/sisohelper.c
@@ -1,164 +1,188 @@
-#include <stdio.h>
+/* Copyright 2014-2026 The PySCF Developers. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/*
+        Author: Bhavnesh Jangid
+*/
+
 #include <complex.h>
-#include <omp.h>
+#include <stddef.h>
 
-void SOCcompute_ss(const double complex* zeff, 
-    const double complex* civecs,
-    double complex* b_data, 
-    const int* alphadetcoupl, 
-    const int* betadetcoupl, 
-    const int* shapearr) {
+#include "siso.h"
 
-    // Reading the shapearr to get the shapes of different arrays
-    // b_data is the output array, 
-    // alphadetcoupl and betadetcoupl are the coupling arrays
-    int b0shp = shapearr[0];
-    int b1shp = shapearr[1]; 
-    int b2shp = shapearr[2]; 
-    int b3shp = shapearr[3];
-    int a0shp = shapearr[4]; 
-    int a1shp = shapearr[5];
-    int nao = shapearr[6];
-    int c1shp = shapearr[7];
-    int c2shp = shapearr[8];
-    int bsize = b0shp * b1shp * b2shp * b3shp;
-    int b1b2b3shp = b1shp * b2shp * b3shp;
-    int b2b3shp = b2shp * b3shp;
+#define SOC_NCOMP 3
+#define LINK_STRIDE 4
 
-    // Initializing the b_data
-    #pragma omp parallel for
-    for (int i = 0; i < b0shp * b1shp * b2shp * b3shp; ++i) {
-        b_data[i] = 0.0 + 0.0 * I;
-    }
-
-    #pragma omp parallel for collapse(4)
-    for (int b0 = 0; b0 < b0shp; ++b0) {
-        for (int b1 = 0; b1 < b1shp; ++b1) {
-            for (int b2 = 0; b2 < b2shp; ++b2) {
-                for (int b3 = 0; b3 < b3shp; ++b3) {
-                    double complex sum = 0.0 + 0.0 * I;
-
-                    for (int aa = 0; aa < a0shp; ++aa) {
-                        int a0_idx = b2 * a0shp * 4 + aa * 4;
-                        int z_idx = b0 * nao * nao + alphadetcoupl[a0_idx] * nao + alphadetcoupl[a0_idx + 1];
-                        int c_idx = b1 * (c1shp * c2shp) + alphadetcoupl[a0_idx + 2] * c2shp + b3;
-                        sum += zeff[z_idx] * civecs[c_idx] * (double)alphadetcoupl[a0_idx + 3];
-                    }
-
-                    for (int ab = 0; ab < a1shp; ++ab) {
-                        int a1_idx = b3 * a1shp * 4 + ab * 4;
-                        int z_idx = b0 * nao * nao + betadetcoupl[a1_idx] * nao + betadetcoupl[a1_idx + 1];
-                        int c_idx = b1 * (c1shp * c2shp) + b2 * c2shp + betadetcoupl[a1_idx + 2];
-                        sum -= zeff[z_idx] * civecs[c_idx] * (double)betadetcoupl[a1_idx + 3];
-                    }
-
-                    // Store the result to b_data
-                    int b_idx = b0 * b1shp * b2shp * b3shp + b1 * b2shp * b3shp + b2 * b3shp + b3;
-                    b_data[b_idx] = sum;
-                }
-            }
-        }
-    }
+static inline size_t h1e_addr(int component, int p, int q, int norb)
+{
+        return ((size_t)component * norb + p) * norb + q;
 }
 
-void SOCcompute_ssp(const double complex* zeff,
-    const double complex* civecs,
-    double complex* b_data,
-    const int* alphadetcoupl,
-    const int* betadetcoupl,
-    const int* shapearr) {
-
-    int b0shp = shapearr[0];
-    int b1shp = shapearr[1]; 
-    int b2shp = shapearr[2]; 
-    int b3shp = shapearr[3];
-    int a0shp = shapearr[4]; 
-    int a1shp = shapearr[5];
-    int nao = shapearr[6];
-    int c1shp = shapearr[7];
-    int c2shp = shapearr[8];
-    int bsize = b0shp * b1shp * b2shp * b3shp;
-    int b1b2b3shp = b1shp * b2shp * b3shp;
-    int b2b3shp = b2shp * b3shp;
-
-    // Initializing the b_data
-    #pragma omp parallel for schedule(static)
-    for (int i = 0; i < bsize; ++i) {
-        b_data[i] = 0.0 + 0.0 * I;
-    }
-
-    #pragma omp parallel for collapse(4)
-    for (int b0 = 0; b0 < b0shp; ++b0) {
-        for (int b1 = 0; b1 < b1shp; ++b1) {
-            for (int b2 = 0; b2 < b2shp; ++b2) {
-                for (int b3 = 0; b3 < b3shp; ++b3) {
-                    double complex sum = 0.0 + 0.0 * I;
-
-                    for (int aa = 0; aa < a0shp; ++aa) {
-                        int a0_idx = b2 * a0shp * 4 + aa * 4;
-                        for (int ab = 0; ab < a1shp; ++ab) {
-                            int a1_idx = b3 * a1shp * 4 + ab * 4;
-                            int z_idx = b0 * nao * nao + betadetcoupl[a1_idx] * nao + alphadetcoupl[a0_idx + 1];
-                            int c_idx = b1 * (c1shp * c2shp) + alphadetcoupl[a0_idx + 2] * c2shp + betadetcoupl[a1_idx + 2];
-                            sum += civecs[c_idx] * zeff[z_idx] * (double)alphadetcoupl[a0_idx + 3] * (double)betadetcoupl[a1_idx + 3];
-                        }
-                    }
-                    // Store the result to b_data
-                    int b_idx = b0 * b1b2b3shp + b1 * b2b3shp + b2 * b3shp + b3;
-                    b_data[b_idx] = sum;
-                }
-            }
-        }
-    }
+static inline size_t ci_addr(int root, int stra, int strb,
+                             int nstra, int nstrb)
+{
+        return ((size_t)root * nstra + stra) * nstrb + strb;
 }
 
-void SOCcompute_ssm(const double complex* zeff,
-    const double complex* civecs,
-    double complex* b_data,
-    const int* alphadetcoupl,
-    const int* betadetcoupl,
-    const int* shapearr) {
-    
-    int b0shp = shapearr[0];
-    int b1shp = shapearr[1]; 
-    int b2shp = shapearr[2]; 
-    int b3shp = shapearr[3];
-    int a0shp = shapearr[4]; 
-    int a1shp = shapearr[5];
-    int nao = shapearr[6];
-    int c1shp = shapearr[7];
-    int c2shp = shapearr[8];
-    int bsize = b0shp * b1shp * b2shp * b3shp;
-    int b1b2b3shp = b1shp * b2shp * b3shp;
-    int b2b3shp = b2shp * b3shp;
+static inline size_t output_addr(int component, int root, int stra, int strb,
+                                 int nroots, int nstra, int nstrb)
+{
+        return (((size_t)component * nroots + root) * nstra + stra)
+               * nstrb + strb;
+}
 
-    // Initialize b_data to zero
-    #pragma omp parallel for
-    for (int i = 0; i < bsize; ++i) {
-        b_data[i] = 0.0 + 0.0 * I;
-    }
+/* Contract the rank-one SOC operator between states with equal total spin. */
+void SISOcontract_same_spin(const double complex *h1e,
+                            const double complex *ci0,
+                            double complex *ci1,
+                            int norb, int nroots,
+                            int nstra1, int nstrb1,
+                            int nstra0, int nstrb0,
+                            int nlinka, int nlinkb,
+                            const int *link_indexa,
+                            const int *link_indexb)
+{
+#pragma omp parallel for collapse(4) schedule(static)
+        for (int component = 0; component < SOC_NCOMP; component++) {
+                for (int root = 0; root < nroots; root++) {
+                        for (int stra = 0; stra < nstra1; stra++) {
+                                for (int strb = 0; strb < nstrb1; strb++) {
+                                        double complex value = 0.0;
 
-    #pragma omp parallel for collapse(4)
-    for (int b0 = 0; b0 < b0shp; ++b0) {
-        for (int b1 = 0; b1 < b1shp; ++b1) {
-            for (int b2 = 0; b2 < b2shp; ++b2) {
-                for (int b3 = 0; b3 < b3shp; ++b3) {
-                    double complex sum = 0.0 + 0.0 * I;
-                    
-                    for (int aa = 0; aa < a0shp; ++aa) {
-                        int a0_idx = b2 * a0shp * 4 + aa * 4;
-                        for (int ab = 0; ab < a1shp; ++ab) {
-                            int a1_idx = b3 * a1shp * 4 + ab * 4;
-                            int z_idx = b0 * nao * nao + alphadetcoupl[a0_idx] * nao + betadetcoupl[a1_idx + 1];
-                            int c_idx = b1 * (c1shp * c2shp) + alphadetcoupl[a0_idx + 2] * c2shp + betadetcoupl[a1_idx + 2];
-                            sum += civecs[c_idx] * zeff[z_idx] * (double)alphadetcoupl[a0_idx + 3] * (double)betadetcoupl[a1_idx + 3];
+                                        const int *linka = link_indexa
+                                                + (size_t)stra * nlinka * LINK_STRIDE;
+                                        for (int link = 0; link < nlinka; link++) {
+                                                const int *entry = linka
+                                                        + link * LINK_STRIDE;
+                                                value += entry[3]
+                                                        * h1e[h1e_addr(component,
+                                                                      entry[0], entry[1],
+                                                                      norb)]
+                                                        * ci0[ci_addr(root, entry[2],
+                                                                      strb, nstra0,
+                                                                      nstrb0)];
+                                        }
+
+                                        const int *linkb = link_indexb
+                                                + (size_t)strb * nlinkb * LINK_STRIDE;
+                                        for (int link = 0; link < nlinkb; link++) {
+                                                const int *entry = linkb
+                                                        + link * LINK_STRIDE;
+                                                value -= entry[3]
+                                                        * h1e[h1e_addr(component,
+                                                                      entry[0], entry[1],
+                                                                      norb)]
+                                                        * ci0[ci_addr(root, stra,
+                                                                      entry[2], nstra0,
+                                                                      nstrb0)];
+                                        }
+
+                                        ci1[output_addr(component, root, stra, strb,
+                                                        nroots, nstra1, nstrb1)] = value;
+                                }
                         }
-                    }
-                    // Store the result to b_data
-                    int b_idx = b0 * b1b2b3shp + b1 * b2b3shp + b2 * b3shp + b3;
-                    b_data[b_idx] = sum;
                 }
-            }
         }
-    }
+}
+
+/* Contract from a ket with spin S to a bra with spin S+1. */
+void SISOcontract_spin_plus(const double complex *h1e,
+                            const double complex *ci0,
+                            double complex *ci1,
+                            int norb, int nroots,
+                            int nstra1, int nstrb1,
+                            int nstra0, int nstrb0,
+                            int nlinka, int nlinkb,
+                            const int *link_indexa,
+                            const int *link_indexb)
+{
+#pragma omp parallel for collapse(4) schedule(static)
+        for (int component = 0; component < SOC_NCOMP; component++) {
+                for (int root = 0; root < nroots; root++) {
+                        for (int stra = 0; stra < nstra1; stra++) {
+                                for (int strb = 0; strb < nstrb1; strb++) {
+                                        double complex value = 0.0;
+                                        const int *linka = link_indexa
+                                                + (size_t)stra * nlinka * LINK_STRIDE;
+                                        const int *linkb = link_indexb
+                                                + (size_t)strb * nlinkb * LINK_STRIDE;
+
+                                        for (int ia = 0; ia < nlinka; ia++) {
+                                                const int *a = linka + ia * LINK_STRIDE;
+                                                for (int ib = 0; ib < nlinkb; ib++) {
+                                                        const int *b = linkb
+                                                                + ib * LINK_STRIDE;
+                                                        value += a[3] * b[3]
+                                                                * h1e[h1e_addr(component,
+                                                                              b[0], a[1],
+                                                                              norb)]
+                                                                * ci0[ci_addr(root, a[2],
+                                                                              b[2], nstra0,
+                                                                              nstrb0)];
+                                                }
+                                        }
+
+                                        ci1[output_addr(component, root, stra, strb,
+                                                        nroots, nstra1, nstrb1)] = value;
+                                }
+                        }
+                }
+        }
+}
+
+/* Contract from a ket with spin S to a bra with spin S-1. */
+void SISOcontract_spin_minus(const double complex *h1e,
+                             const double complex *ci0,
+                             double complex *ci1,
+                             int norb, int nroots,
+                             int nstra1, int nstrb1,
+                             int nstra0, int nstrb0,
+                             int nlinka, int nlinkb,
+                             const int *link_indexa,
+                             const int *link_indexb)
+{
+#pragma omp parallel for collapse(4) schedule(static)
+        for (int component = 0; component < SOC_NCOMP; component++) {
+                for (int root = 0; root < nroots; root++) {
+                        for (int stra = 0; stra < nstra1; stra++) {
+                                for (int strb = 0; strb < nstrb1; strb++) {
+                                        double complex value = 0.0;
+                                        const int *linka = link_indexa
+                                                + (size_t)stra * nlinka * LINK_STRIDE;
+                                        const int *linkb = link_indexb
+                                                + (size_t)strb * nlinkb * LINK_STRIDE;
+
+                                        for (int ia = 0; ia < nlinka; ia++) {
+                                                const int *a = linka + ia * LINK_STRIDE;
+                                                for (int ib = 0; ib < nlinkb; ib++) {
+                                                        const int *b = linkb
+                                                                + ib * LINK_STRIDE;
+                                                        value += a[3] * b[3]
+                                                                * h1e[h1e_addr(component,
+                                                                              a[0], b[1],
+                                                                              norb)]
+                                                                * ci0[ci_addr(root, a[2],
+                                                                              b[2], nstra0,
+                                                                              nstrb0)];
+                                                }
+                                        }
+
+                                        ci1[output_addr(component, root, stra, strb,
+                                                        nroots, nstra1, nstrb1)] = value;
+                                }
+                        }
+                }
+        }
 }
