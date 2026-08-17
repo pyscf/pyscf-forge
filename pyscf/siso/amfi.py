a354e5225ee589b66748b620709069f9e8ab4b0d
JangidBhavnesh <bhuvnesh.jangid10@gmail.com>
2026-08-17 17:04:53 -0500

    More optimized version for SOC integral computation

--- pyscf/siso/amfi.py
+++ pyscf/siso/amfi.py
@@ -24,8 +24,8 @@ import copy
 import scipy
 import numpy as np
 from functools import reduce
-from pyscf import lib, ao2mo
-from pyscf.scf import atom_hf
+from pyscf import lib
+from pyscf.scf import atom_hf, jk
 from pyscf.data import elements
 
 logger = lib.logger
@@ -207,28 +207,50 @@ def compute_soc2e_jk(pmol, dm0, mo1, mo3):
     # vk=np.einsum('yijkl,jk->yil', hso2e, dm0)
     # vk+=np.einsum('yijkl,li->ykj', hso2e, dm0)
 
-    nao = dm0.shape[0]
+    # Previous AO2MO implementation:
+    # nao = dm0.shape[0]
+    #
+    # vj = np.zeros((3, nao, nao))
+    # hso2e_ = ao2mo.general(
+    #     pmol, (mo3, mo1, mo3, mo1), intor='int2e_p1vxp1', aosym='s1',
+    #     comp=3, compact=True)
+    # vj = np.asarray([
+    #     np.einsum(
+    #         'ijkl,lk->ij', ao2mo.restore(1, hso2e_[i], nao), dm0)
+    #     for i in range(3)])
+    #
+    # hso2e_ = ao2mo.general(
+    #     pmol, (mo3, mo1, mo1, mo3), intor='int2e_p1vxp1', aosym='s1',
+    #     comp=3, compact=True)
+    # vk = np.zeros_like(vj)
+    # for i in range(3):
+    #     hso2etemp = ao2mo.restore(1, hso2e_[i], nao)
+    #     vk[i] = np.einsum('ijkl,jk->il', hso2etemp, dm0)
+    #     vk[i] += np.einsum('ijkl,li->kj', hso2etemp, dm0)
+    # del hso2e_
 
-    # Directly using the ao2mo.general to get the 2e integrals in the cGTO basis
+    assert not np.iscomplexobj(dm0), 'compute_soc2e_jk requires a real density'
 
-    # Columb like matrix
-    vj = np.zeros((3, nao, nao))
-    hso2e_ = ao2mo.general(pmol, (mo3, mo1, mo3, mo1),
-                           intor='int2e_p1vxp1',aosym='s1',comp=3,compact=True)
-    vj = np.asarray([np.einsum('ijkl,lk->ij', ao2mo.restore(1, hso2e_[i], nao),
-                                   dm0) for i in range(3)])
+    # Instead of transforming and storing the full SOC integral tensor in the
+    # cGTO basis before contracting it with the density, using PySCF's direct JK
+    # builder.  The density is transformed to the primitive basis and contracted
+    # as the integral shell quartets are generated.  This avoids the temporary
+    # HDF5 file and the O(nao**4) tensor created by ao2mo.general.
 
-    # Exchange like matrix
-    hso2e_ = ao2mo.general(pmol, (mo3, mo1, mo1, mo3),
-                         intor='int2e_p1vxp1',aosym='s1',comp=3,compact=True)
+    dm_j = reduce(np.dot, (mo1, dm0, mo3.T))
+    dm_k1 = reduce(np.dot, (mo1, dm0, mo1.T))
+    dm_k2 = reduce(np.dot, (mo3, dm0, mo3.T))
+    dms = (dm_j, dm_k1, dm_k2)
+    scripts = ('ijkl,lk->ij', 'ijkl,jk->il', 'ijkl,li->kj')
+    vj_ao, vk1_ao, vk2_ao = jk.get_jk(
+        pmol, dms, scripts=scripts, intor='int2e_p1vxp1', aosym='s1',
+        comp=3, hermi=0)
 
-    vk = np.zeros_like(vj)
-    for i in range(3):
-        hso2etemp = ao2mo.restore(1, hso2e_[i], nao)
-        vk[i] = np.einsum('ijkl,jk->il', hso2etemp, dm0)
-        vk[i] += np.einsum('ijkl,li->kj', hso2etemp, dm0)
+    vj = np.einsum('pi,xpq,qj->xij', mo3, vj_ao, mo1, optimize=True)
+    vk = np.einsum('pi,xpq,qj->xij', mo3, vk1_ao, mo3, optimize=True)
+    vk += np.einsum('pi,xpq,qj->xij', mo1, vk2_ao, mo1, optimize=True)
 
-    del hso2e_
+    del dm_j, dm_k1, dm_k2, vj_ao, vk1_ao, vk2_ao
 
     return vj, vk
 
