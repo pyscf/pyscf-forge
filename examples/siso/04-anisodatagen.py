347ef61744e6290a89d2405547ff9f25575b9a94
JangidBhavnesh <bhuvnesh.jangid10@gmail.com>
2026-08-18 16:30:20 -0500

    update the example with smaller systems

--- examples/siso/04-anisodatagen.py
+++ examples/siso/04-anisodatagen.py
@@ -12,29 +12,30 @@ from pyscf.siso.anisoaddons import generate_aniso_data, write_aniso_file
 # 1. Define the molecule
 mol = gto.Mole()
 mol.atom='''
-Dy 0 0 0
-O 0 0 1.74'''
-mol.basis= {'Dy': 'ano@6s5p3d1f', 'O': 'ano@2s1p'} # ANO-MB
+Cr 0 0 0
+H 0 0 1.69'''
+mol.basis= basis = {'Cr': 'ano@4s3p1d', 'H' : 'ano@1s'} # ANO-MB
 mol.spin = 5
-mol.charge = 1
+mol.charge = 0
 mol.verbose = 4
-mol.max_memory = 120000
-mol.output = 'DyO.out'
+mol.max_memory = 10000
+mol.output = 'CrH.out'
 mol.build()
 
-# 2. SCF calculation
+# 2. SCF calculation:
 mf = scf.RHF(mol).sfx2c1e().density_fit()
-mf.chkfile='DyO.chk'
 mf.init_guess = 'atom'
+mf.max_cycle = 10
 mf.kernel()
 
 # 3. Active space selection via AVAS:
-mo_coeff = avas.kernel(mf, ['Dy 4f',], minao=mol.basis)[2]
+mo_coeff = avas.kernel(mf, ['Cr 3d', 'Cr 4s', 'H 1s'], minao=mol.basis)[2]
 
 # 4. State-averaged CASSCF followed by state interaction:
-# Note this is just for demonstration purposes. Use appropirate number of states for production level computations.
-modelspace = [(3,6),]
-mc = mcscf.CASSCF(mf, 7, 9)
+# Note this is just for demonstration purposes.
+# Use appropirate number of states for production level computations.
+modelspace = [(5,4), (5,6)]
+mc = mcscf.CASSCF(mf, 7, 7)
 mc = siso.sacasscf_solver(mc, modelspace)
 mc.max_cycle_macro = 200
 mc.kernel(mo_coeff)
@@ -48,10 +49,10 @@ mysiso.kernel()
 # 6. Generate the data for the single_aniso module:
 mydata = generate_aniso_data(
     mol, mc, mysiso=mysiso, origin='CHARGE_CENTER', ham='DKH')
-write_aniso_file('DyO.aniso', data = mydata, backend='Orca')
+write_aniso_file('CrH.aniso', data = mydata, backend='Orca')
 
 # 7. To run the single_aniso module, you will need to create the input file for the single_aniso, in addition to
-# the aniso data file created above (DyO.aniso) and binaries of the single_aniso module. Currently you can access
+# the aniso data file created above (CrH.aniso) and binaries of the single_aniso module. Currently you can access
 # these via OpenMolcas or Orca.
 # For more details on how to use SingleANISO: see:
 # https://www.faccts.de/docs/orca/6.0/manual/contents/detailed/single_aniso.html
