c341ae3976c8ec642caf502add1632fe90334e03
jiseong_QClab <fark4308@snu.ac.kr>
2026-08-19 23:32:27 +0900

    GBCI gradient test Update

--- pyscf/grad/test/test_gbci_grad.py
+++ pyscf/grad/test/test_gbci_grad.py
@@ -7,10 +7,12 @@
 import unittest
 from unittest import mock
 
+import numpy
+
 from pyscf import gbci, gto, lib, scf
 from pyscf.grad import gbci as gbci_grad
 from pyscf.grad import rohf_gbci as rohf_gbci_grad
-from pyscf.grad.gbci import _bath_rotation_zvec
+from pyscf.grad.gbci import _solve_bath_cphf
 from pyscf.grad.rohf_casci import _solve_rohf_adjoint
 
 
@@ -24,29 +26,29 @@ LIH_GRAD_Z = 0.01830102797453953
 LIF_GRAD_Z = 0.03548531842959424
 LIH_TRIPLET_ROHF_GRAD_Z = 0.0294477687822165
 LIF_TRIPLET_ROHF_GRAD_Z = 0.04667256084318572
-LIH_BATH_RESPONSE_FP = (
-    -1.627492829776010e-05,
-    1.119788583093171e-04,
-    2.120511245200006e-04,
+LIH_BATH_RESPONSE_NORM = (
+    2.472393816847649e-06,
+    1.719473808106923e-05,
+    4.312046659752552e-05,
 )
-LIF_BATH_RESPONSE_FP = (
+LIF_BATH_RESPONSE_NORM = (
     0.0,
-    2.216177220066382e-10,
-    2.631660466514099e-05,
-    -1.611782286360561e-03,
-    -4.668488171487795e-04,
+    2.128160330971092e-10,
+    2.207031852017983e-05,
+    1.815964468198721e-03,
+    1.764166191368899e-03,
 )
-LIH_TRIPLET_ROHF_BATH_RESPONSE_FP = (
-    -1.934943396922866e-03,
-    8.244476143925915e-03,
-    2.983600786582193e-02,
+LIH_TRIPLET_ROHF_BATH_RESPONSE_NORM = (
+    2.739090326756687e-04,
+    1.353383205145588e-03,
+    5.266287789012821e-03,
 )
-LIF_TRIPLET_ROHF_BATH_RESPONSE_FP = (
+LIF_TRIPLET_ROHF_BATH_RESPONSE_NORM = (
     0.0,
-    6.210682700180350e-09,
-    2.158303995930926e-05,
-    -4.669944849630451e-03,
-    -5.302987569270201e-03,
+    5.165116275955623e-09,
+    2.155107748521304e-05,
+    8.977611205020856e-03,
+    1.933671822164339e-02,
 )
 LIH_TRIPLET_ROHF_RESPONSE_FP = 0.12102737231463209
 LIF_TRIPLET_ROHF_RESPONSE_FP = 0.367922010414346
@@ -85,12 +87,13 @@ def get_gbci_grad(atom, ncas, nelecas, spin=0, mf_cls=scf.RHF):
         mc.fcisolver.conv_tol = 1e-10
         mc.run()
 
-        bath_response_fp = []
+        bath_response_norm = []
         rohf_response_fp = []
 
         def record_bath_response(*args, **kwargs):
-            response = _bath_rotation_zvec(*args, **kwargs)
-            bath_response_fp.extend(float(lib.fp(x)) for x in response[1])
+            response = _solve_bath_cphf(*args, **kwargs)
+            if response.shape[1] == mc.ncore:
+                bath_response_norm.append(float(numpy.linalg.norm(response)))
             return response
 
         def record_rohf_response(*args, **kwargs):
@@ -99,14 +102,12 @@ def get_gbci_grad(atom, ncas, nelecas, spin=0, mf_cls=scf.RHF):
             return response
 
         with mock.patch.object(
-                gbci_grad, "_bath_rotation_zvec",
+                gbci_grad, "_solve_bath_cphf",
                 side_effect=record_bath_response), mock.patch.object(
-                    rohf_gbci_grad, "_bath_rotation_zvec",
-                    side_effect=record_bath_response), mock.patch.object(
                     rohf_gbci_grad, "_solve_rohf_adjoint",
                     side_effect=record_rohf_response):
             grad = mc.nuc_grad_method().kernel()
-        return grad, bath_response_fp, rohf_response_fp
+        return grad, bath_response_norm, rohf_response_fp
     finally:
         if mc is not None:
             close_gbci_resources(mc)
@@ -114,47 +115,47 @@ def get_gbci_grad(atom, ncas, nelecas, spin=0, mf_cls=scf.RHF):
 
 
 class KnownValues(unittest.TestCase):
-    def assert_response_fp(self, response_fp, reference, places=10):
-        self.assertEqual(len(response_fp), len(reference))
-        for value, ref in zip(response_fp, reference):
+    def assert_response(self, response, reference, places):
+        self.assertEqual(len(response), len(reference))
+        for value, ref in zip(response, reference):
             self.assertAlmostEqual(value, ref, places)
 
     def test_lih_2o2e(self):
-        grad, bath_response_fp, rohf_response_fp = get_gbci_grad(
+        grad, bath_response_norm, rohf_response_fp = get_gbci_grad(
             f"Li 0 0 0; H 0 0 {BOND_LENGTH}", 2, (1, 1))
         self.assertAlmostEqual(float(grad[0, 2]), LIH_GRAD_Z, 9)
-        self.assert_response_fp(
-            bath_response_fp, LIH_BATH_RESPONSE_FP, places=6)
+        self.assert_response(
+            bath_response_norm, LIH_BATH_RESPONSE_NORM, places=10)
         self.assertEqual(rohf_response_fp, [])
 
     def test_lif_4o4e(self):
-        grad, bath_response_fp, rohf_response_fp = get_gbci_grad(
+        grad, bath_response_norm, rohf_response_fp = get_gbci_grad(
             f"Li 0 0 0; F 0 0 {BOND_LENGTH}", 4, (2, 2))
         self.assertAlmostEqual(float(grad[0, 2]), LIF_GRAD_Z, 9)
-        self.assert_response_fp(
-            bath_response_fp, LIF_BATH_RESPONSE_FP, places=6)
+        self.assert_response(
+            bath_response_norm, LIF_BATH_RESPONSE_NORM, places=10)
         self.assertEqual(rohf_response_fp, [])
 
     def test_lih_triplet_rohf_2o2e(self):
-        grad, bath_response_fp, rohf_response_fp = get_gbci_grad(
+        grad, bath_response_norm, rohf_response_fp = get_gbci_grad(
             f"Li 0 0 0; H 0 0 {BOND_LENGTH}", 2, (1, 1),
             spin=2, mf_cls=scf.ROHF)
         self.assertAlmostEqual(float(grad[0, 2]), LIH_TRIPLET_ROHF_GRAD_Z, 9)
-        self.assert_response_fp(
-            bath_response_fp, LIH_TRIPLET_ROHF_BATH_RESPONSE_FP, places=6)
-        self.assert_response_fp(
-            rohf_response_fp, (LIH_TRIPLET_ROHF_RESPONSE_FP,))
+        self.assert_response(
+            bath_response_norm, LIH_TRIPLET_ROHF_BATH_RESPONSE_NORM, places=10)
+        self.assert_response(
+            rohf_response_fp, (LIH_TRIPLET_ROHF_RESPONSE_FP,), places=10)
 
     def test_lif_triplet_rohf_4o4e(self):
-        grad, bath_response_fp, rohf_response_fp = get_gbci_grad(
+        grad, bath_response_norm, rohf_response_fp = get_gbci_grad(
             f"Li 0 0 0; F 0 0 {BOND_LENGTH}", 4, (2, 2),
             spin=2, mf_cls=scf.ROHF)
         self.assertAlmostEqual(
             float(grad[0, 2]), LIF_TRIPLET_ROHF_GRAD_Z, 9)
-        self.assert_response_fp(
-            bath_response_fp, LIF_TRIPLET_ROHF_BATH_RESPONSE_FP, places=6)
-        self.assert_response_fp(
-            rohf_response_fp, (LIF_TRIPLET_ROHF_RESPONSE_FP,))
+        self.assert_response(
+            bath_response_norm, LIF_TRIPLET_ROHF_BATH_RESPONSE_NORM, places=10)
+        self.assert_response(
+            rohf_response_fp, (LIF_TRIPLET_ROHF_RESPONSE_FP,), places=10)
 
 
 if __name__ == "__main__":
